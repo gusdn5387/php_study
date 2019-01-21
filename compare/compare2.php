088f182d23cc4b7d032880ead40c30b19ffc661f
h0225w <31609042+gusdn5387@users.noreply.github.com>
2019-01-21 18:03:16 +0900

    비교
    비교 연산자

---
+++ compare/compare2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "1!=2 : ";
+    var_dump(1!=2);
+    echo '<br/>';
+    echo "1!=1 : ";
+    var_dump(1!==1);  
+    echo '<br/>';
+    echo '"one"!="two" : ';
+    var_dump("one"!="two");  
+    echo '<br/>';
+    echo '"one"!="one" : ';
+    var_dump("one"!="one");  
+    echo '<br />';
+    ?>
+</body>
+</html>
