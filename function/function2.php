30e2d1a4a7ee4ef3d77232558f66bf7a12c38eee
h0225w <31609042+gusdn5387@users.noreply.github.com>
2019-01-21 18:46:43 +0900

    함수
    function

---
+++ function/function2.php
@@ -0,0 +1,13 @@
+<?php
+function get_member($arg) {
+    return $arg;
+}
+
+function plus($arg1, $arg2) {
+    return $arg1 + $arg2;
+}
+
+echo get_member("member1");
+echo "<br/>";
+echo plus(1,5);
+?>
