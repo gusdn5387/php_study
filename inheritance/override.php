ecf9ddac3c4fe268bf3c38f6add2e9e5e16b5552
h0225w <31609042+gusdn5387@users.noreply.github.com>
2019-01-23 16:29:59 +0900

    상속
    접근제어자, 상속금지, OVERRIDE

---
+++ inheritance/override.php
@@ -0,0 +1,16 @@
+<?php
+class ParentClass {
+    function callMethod($param) {
+        echo "<h1>Parent {$param}</h1>";
+    }
+}
+
+class ChildClass extends ParentClass {
+    function callMethod($param) {
+        parent::callMethod($param);
+        echo "<h1>Child {$param}</h1>";
+    }
+}
+$obj = new ChildClass();
+$obj->callMethod('method');
+?>
